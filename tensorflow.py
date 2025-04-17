3fbbe6c211d9135b187b53bb4bac720b8308fa08
Abdullah Khalif <65001115+akhalif027@users.noreply.github.com>
2025-04-17 08:38:20 -0400

    Refactored CNN code

--- tensorflow.py
+++ tensorflow.py
@@ -1,22 +1,19 @@
 import tensorflow as tf
 import numpy as np
-import pandas as pd 
+import os
 import cv2 
-import os 
 import matplotlib.pyplot as plt 
 from tensorflow import keras 
 from PIL import Image
 
 #Data Visualization
 normal_image = cv2.imread('/kaggle/input/chest-xray-pneumonia/chest_xray/train/NORMAL/IM-0115-0001.jpeg')
-plt.imshow(normal_image)
-plt.title('Normal Lungs Sample #1')
-plt.show()
+plt.title('Normal Lungs Sample')
+plt.imshow()
 
-pneumonia_image1 = cv2.imread('/kaggle/input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA/person1000_bacteria_2931.jpeg')
-plt.imshow(pneumonia_image1)
-plt.title('Pneumonia Lungs Sample #1')
-plt.show()
+pneumonia_image = cv2.imread('/kaggle/input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA/person1000_bacteria_2931.jpeg')
+plt.title('Pneumonia Lungs Sample')
+plt.imshow()
 
 #Data Preprocessing
 
@@ -25,24 +22,25 @@ print(f'Normal Lungs #2 Shape {normal_image2.shape}')
 
 normal_images = os.listdir('/kaggle/input/chest-xray-pneumonia/chest_xray/train/NORMAL/')
 pneumonia_images = os.listdir('/kaggle/input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA/')
+len(normal_images + pneumonia_images)
 
 #Normalizing the dimensions of the dataset's images. 
-reprocessed_images = []
 def image_reprocessing(path, images):
+    reprocessed_images = []
     for image in images:
         image = Image.open(path + image)
         image = image.resize((128,128))
         image = image.convert('RGB')
         image = np.array(image)
         image = image / 255.0
-
         reprocessed_images.append(image)
+        
+    return reprocessed_images
 
 new_normal = image_reprocessing(('/kaggle/input/chest-xray-pneumonia/chest_xray/train/NORMAL/'),normal_images)
 new_pneumonia = image_reprocessing(('/kaggle/input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA/'),pneumonia_images)
 
-#Checking if all images are present and the standard dimension is used. 
-len(reprocessed_images)
+#Checking if the standard dimension is applied correctly. 
 print(f' Adjusted image shape : {reprocessed_images[0].shape}')
 
 #Preparing images for model training via one-hot encoding and array convertion. 
@@ -51,7 +49,10 @@ pneumonia_training_labels = [1] * len(pneumonia_images)
 training_labels = normal_training_labels + pneumonia_training_labels 
 len(training_labels)
 
-X = np.array(reprocessed_images)
+training_imagews = new_normal + new_pneumonia
+len(training_images)
+
+X = np.array(training_images)
 Y = np.array(training_labels)
 
 #Building the model
@@ -80,30 +81,32 @@ model.compile('adam',loss= tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 hist = model.fit(X, Y, validation_split=0.2, verbose=1, epochs=5)
 
 #Plotting training accuracy and loss rate. 
-plt.plot(hist.history['loss'])
-plt.plot(hist.history['val_loss'])
+plt.plot(hist.history['loss'], label= "Training Loss")
+plt.plot(hist.history['val_loss'], label= "Validation Loss")
+plt.legend()
+plt.grid(True)
+plt.title('Training Loss VS Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.show()
 
-plt.plot(hist.history['accuracy'])
-plt.plot(hist.history['val_accuracy'])
+plt.plot(hist.history['accuracy'], label= 'Training Accuracy')
+plt.plot(hist.history['val_accuracy'], label= 'Validation Accuracy')
+plt.legend()
+plt.grid(True)
+plt.title('Training Accuracy VS Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.show()
 
 #Testing the model
 #Preprocessing testing images procedure similar to training images. 
 normal_images = os.listdir('/kaggle/input/chest-xray-pneumonia/chest_xray/test/NORMAL/')
 pneumonia_images = os.listdir('/kaggle/input/chest-xray-pneumonia/chest_xray/test/PNEUMONIA/')
-
-testing_images = []
-def image_reprocessing(path, images):
-    for image in images:
-        image = Image.open(path + image)
-        image = image.resize((128,128))
-        image = image.convert('RGB')
-        image = np.array(image)
-        image = image / 255.0
-
-        testing_images.append(image)
         
 new_normal = image_reprocessing(('/kaggle/input/chest-xray-pneumonia/chest_xray/test/NORMAL/'),normal_images)
 new_pneumonia = image_reprocessing(('/kaggle/input/chest-xray-pneumonia/chest_xray/test/PNEUMONIA/'),pneumonia_images)
+testing_images = new_normal + new_pneumonia
 len(testing_images)
 
 normal_testing_labels = len(normal_images) * [0]
